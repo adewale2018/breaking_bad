b0cc9d76440ae321bceaa3b2418301e70ca227a5
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-07-04 02:46:51 +0100

    set up the project

---
+++ src/components/ui/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../../images/logo.png";
+
+const Header = () => {
+  return (
+    <header className='center'>
+      <img src={logo} alt='breaking bad brand logo' />
+    </header>
+  );
+};
+
+export default Header;
